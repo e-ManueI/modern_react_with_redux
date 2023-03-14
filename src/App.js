349cfa115c6eda0118a22814ec7d76a7bd6783ba
Emmanuel T <19648538+e-ManueI@users.noreply.github.com>
2023-03-14 20:48:27 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+import Field from "./components/field";
+import Translate from "./components/translate";
+import Languages from "./components/languages";
+
+export default function App() {
+  const [language, setLanguage] = useState('es');
+  const [text, settext] = useState('');
+  return (
+    <div>
+      <Field />
+      <Languages />
+      <hr />
+      <Translate />
+    </div>
+  );
+}
